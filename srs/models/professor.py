3c9e66658269136079fa7f334a606b1c978cdf4f
MAYA-P-code <mayaaboamer25@gmail.com>
2025-12-05 19:30:11 +0200

    view all registered students function
    Added method to view all registered students for a course.

--- srs/models/professor.py
+++ srs/models/professor.py
@@ -1,5 +1,18 @@
+from app import get_db
+
 class Professor:
     def __init__(self, pID: str, pname: str, password: str):
         self.pID = pID       
         self.pname = pname   
         self.password = password  
+
+ def view_all_registered_students(self, course_id: str):
+      db = get_db()
+
+      rows = db.execute(
+       (course_id, self.pID),
+        ).fetchall()
+
+students = [dict(row) for row in rows]
+
+        return students

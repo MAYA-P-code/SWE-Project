7d64eeabdb670f989b0055cc7acc951f77ad7add
Hazem Refaat <s-hazem.refaat@zewailcity.edu.eg>
2025-12-20 14:18:44 +0200

    Revert "fixed professor controller"

--- srs/controllers/professor_controller.py
+++ srs/controllers/professor_controller.py
@@ -1,38 +1,38 @@
 from flask import Blueprint, render_template, session, request
+from flask_login import login_required, current_user
 from srs.models.professor import Professor
 from srs.repositories.professorRepo import professorRepo
-from srs.db import get_db 
 
 professor_bp = Blueprint("professor", __name__, url_prefix="/professor")
 
 @professor_bp.route("/courses/<c_id>/students")
+@login_required
 def get_students_in_course(c_id):
-    p_id = session["userID"]
-    p_name = session.get("username", "")
-    db = get_db()
-    repo = professorRepo(db, p_id) 
-
-    prof = Professor(pID=p_id, pname=p_name, password="")
-    students = repo.get_students_in_course(c_id)
+    prof = current_user
+    students = professorRepo.get_students_in_course(c_id)
     return render_template(
         "view_students.html",
         professor=prof,
         course_id=c_id,
         students=students,
     )
-@professor_bp.route("/assign_grade", methods=["POST"])
+
+@professor_bp.route('/assign_grade', methods=['POST'])
+@login_required
 def assign_grade():
-    professor_id = session.get('userID')
-    if not professor_id:
-        return "Not logged in"
+    professor_id = current_user.pID
     
     student_id = request.form.get("student_id")
     course_id = request.form.get("course_id")
     grade = request.form.get("grade")
     
-    db = get_db()
-    repo = professorRepo(db, professor_id)
-    result = repo.assign_grade(student_id, course_id, grade)
-
+    result = professorRepo.assign_grade(student_id, course_id, grade)
 
     return result
+
+def view_courses():
+    professor_id = current_user.pID
+    
+    courses = professorRepo.get_courses(professor_id)
+
+    return render_template("ProfessorHome.html", courses=courses)
